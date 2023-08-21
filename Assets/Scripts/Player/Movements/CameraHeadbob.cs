f8652dc92b6972afb1ae5d3b05a98236dab4f177
Antoine <45072872+SlayerOfShadow@users.noreply.github.com>
2023-08-21 10:24:40 +0200

    modified camera headbob

--- Assets/Scripts/Player/Movements/CameraHeadbob.cs
+++ Assets/Scripts/Player/Movements/CameraHeadbob.cs
@@ -6,15 +6,16 @@ public class CameraHeadbob : MonoBehaviour
 {
     Player player;
 
-    [SerializeField] Vector2 walkAmount = new Vector2(0.02f, 0.01f);
-    [SerializeField] Vector2 runAmount = new Vector2(0.04f, 0.02f);
-    [SerializeField] Vector2 crouchAmount = new Vector2(0.01f, 0.005f);
-    Vector2 amount;
+    [SerializeField] float walkAmount = 0.01f;
+    [SerializeField] float runAmount = 0.03f;
+    [SerializeField] float crouchAmount = 0.005f;
+    float amount;
     [SerializeField] float walkFrequency = 12f;
-    [SerializeField] float runFrequency = 16f;
+    [SerializeField] float runFrequency = 14f;
     [SerializeField] float crouchFrequency = 8f;
     float frequency;
     [SerializeField] float smooth = 20f;
+    [SerializeField] float returnSpeed = 10f;
     Vector3 startPosition;
 
     void Start()
@@ -48,8 +49,7 @@ public class CameraHeadbob : MonoBehaviour
     Vector3 StartHeadbob()
     {
         Vector3 headbobPosition = Vector3.zero;
-        headbobPosition.y += Mathf.Lerp(headbobPosition.y, Mathf.Sin(Time.time * frequency) * amount.x, smooth * Time.deltaTime);
-        headbobPosition.x += Mathf.Lerp(headbobPosition.x, Mathf.Cos(Time.time * frequency * 0.5f) * amount.y, smooth * Time.deltaTime);
+        headbobPosition.y += Mathf.Lerp(headbobPosition.y, Mathf.Sin(Time.time * frequency) * amount, smooth * Time.deltaTime);
         transform.localPosition += headbobPosition;
         return headbobPosition;
     }
@@ -60,6 +60,6 @@ public class CameraHeadbob : MonoBehaviour
         {
             return;
         }
-        transform.localPosition = Vector3.Lerp(transform.localPosition, startPosition, 1 * Time.deltaTime);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, startPosition, returnSpeed * Time.deltaTime);
     }
 }
