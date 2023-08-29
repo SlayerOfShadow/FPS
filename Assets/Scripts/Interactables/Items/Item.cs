265da04b869bb3765bdbf6b8b9ea876b8bb41eec
Antoine <45072872+SlayerOfShadow@users.noreply.github.com>
2023-08-29 15:55:05 +0200

    cleaned code

--- Assets/Scripts/Interactables/Items/Item.cs
+++ Assets/Scripts/Interactables/Items/Item.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : Interactable
 {
     Player player;
-
+    
     [SerializeField] GameObject inventoryItem;
 
     void Start()
